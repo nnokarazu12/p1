65f2672dd4b3dca2f0503f2063a2d5260af94480
nnokarazu12 <nnokarazu@gmail.com>
2022-06-09 15:34:16 -0400

    style change

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const routes = require('./routes/routes');
 const port = 3000;
 
 app.use(express.json());
+app.use(express.static(__dirname + '/styles'));
 app.set('view engine', 'hbs');
 app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
